c83d7fde2390887c1207667958b9fc57d70d29de
Suraj <suraj_2101cs75@iitp.ac.in>
2022-07-27 21:28:19 +0530

    bubble sort

--- sortingtechniques.c
+++ sortingtechniques.c
@@ -79,3 +79,11 @@ void selectionsort(int arr[], int n)
         swap(&arr[min_idx], &arr[i]); 
     } 
 }
+void bubblesort(int arr[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (arr[j] > arr[j + 1])
+                swap(arr[j], arr[j + 1]);
+}
